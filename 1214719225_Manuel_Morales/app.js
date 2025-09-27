34ee043ff0dbf9e5c98dfb734b736e5fa43e2993
Manuelhp24 <manuel2412hm@gmail.com>
2025-09-27 11:50:41 -0500

    Fase 2: Implementadas funciones para ingresos y gastos

--- 1214719225_Manuel_Morales/app.js
+++ 1214719225_Manuel_Morales/app.js
@@ -7,3 +7,20 @@ function mostrarBalance() {
     console.log(`Hola, ${nombreUsuario}, tu saldo es de: $${saldo}. Has gastado: $${gastosTotales}.`);
 }
 mostrarBalance();
+
+
+
+const registrarIngreso = function(montoIngreso) {
+    saldo += montoIngreso;
+    console.log(`Ingreso de $${montoIngreso} registrado. Nuevo saldo: $${saldo}.`);
+}
+
+const registrarGasto = (montoGasto) => {
+    if (montoGasto <= saldo) {
+        saldo -= montoGasto;
+        gastosTotales += montoGasto;
+        console.log(`Gasto de $${montoGasto} registrado. Nuevo saldo: $${saldo}.`);
+    } else {
+        console.log("Fondos insuficientes.");
+    }
+}
